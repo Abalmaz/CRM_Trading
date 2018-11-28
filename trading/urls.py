647bcb18b1f641d6e6d295ced343b318ca631f82
Abalmaz <yana.abalmaz@gmail.com>
2018-11-28 11:42:00 +0200

    Created home page with info about current company

--- trading/urls.py
+++ trading/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from trading import views
 
 urlpatterns = [
-    path('', views.CompanyListView.as_view(), name='home'),
+    path('', views.home, name='home'),
     path('<int:pk>/', views.CompanyDetailView.as_view(), name='company'),
     path('set/<int:pk>', views.set_session_company, name='set_company'),
     path('update/<int:pk>', views.CompanyUpdateView.as_view(), name='update'),
--- trading/views.py
+++ trading/views.py
@@ -1,21 +1,17 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.views import View
 from django.views.generic import DetailView
 from django.views.generic.edit import UpdateView
-from django.views.generic.list import ListView
 
 from trading.mixins import GroupRequiredMixin
 from trading.models import Company
 
 
-class CompanyListView(LoginRequiredMixin, ListView):
-    login_url = 'login'
-    model = Company
-    template_name = 'trading/company_list.html'
-    context_object_name = 'companies'
-
-    def get_queryset(self):
-        return Company.objects.filter(employees=self.request.user)
+@login_required(login_url='login')
+def home(request):
+    return render(request=request, template_name='trading/home.html')
 
 
 class CompanyDetailView(DetailView):
